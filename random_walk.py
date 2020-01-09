b730c982b647cb7af81ce9656882fc540a168161
Jonatan Cerwall <jcervall@kth.se>
2020-01-09 15:37:08 +0100

    random walk kernel

---
+++ random_walk.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.metrics.pairwise import rbf_kernel
+
+def random_walk(M, t=1):
+    K = rbf_kernel(M, gamma=0.55)
+
+    D = np.diag([1/np.sum(row) for row in K])
+    D2 = np.diag([1/np.math.sqrt(x) for x in np.diag(D)])
+    L = D2 @ K @ D2
+
+    L_tilde = np.linalg.matrix_power(L, t)
+    D_tilde = np.diag([1/x for x in np.diag(L_tilde)])
+
+    D_tilde1 = np.diag([np.math.sqrt(x) for x in np.diag(D_tilde)])
+    D1 = np.diag([np.math.sqrt(x) for x in np.diag(D)])
+    P_t = np.linalg.matrix_power((np.linalg.inv(D) @ K), t)
+
+    K_tilde = D_tilde1 @ D1 @ P_t @ D2 @ D_tilde1
+
+    return K_tilde
+
+
+
+
+
