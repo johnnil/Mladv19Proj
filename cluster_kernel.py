4bdee8cf01eb82acd1f95c7e064d262be33d62d4
Emma Nimstad <emma@nimstad.net>
2020-01-10 15:22:29 +0100

    Fix cluster kernel.

--- cluster_kernel.py
+++ cluster_kernel.py
@@ -7,7 +7,7 @@ import numpy as np
 from numpy.linalg import eig
 from sklearn.metrics.pairwise import rbf_kernel
 
-def cluster_kernel(X, K):
+def cluster_kernel(X):
     """
     1. As before, compute the RBF matrix K from both labeled and unlabeled
     points (this time with 1 on the diagonal and not 0) and D, the diagonal
@@ -15,7 +15,7 @@ def cluster_kernel(X, K):
     """
     n = len(X)
     # Affinity matrix using RBF kernel
-    A = rbf_kernel(K, gamma=0.55)
+    A = rbf_kernel(X, gamma=0.55)
     # Set diagonal to 1
     A[np.arange(n), np.arange(n)] = 1
     # Let D be a diagonal matrix with diagonal elements equal to the sum of the rows (or the columns)
