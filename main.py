4bfe8cf55b4353f04c3ca2714e7a3494926f6825
Emma Nimstad <emma@nimstad.net>
2020-01-11 14:37:56 +0100

    Complete test framework. But still some bug.

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ def evaluate_kernel(x_labeled, x_unlabeled, x_test, y, y_test, kernel):
     :param kernel: kernel function that should be evaluated, kernel(x) -> array with shape=(n, n)
     :return: fraction of test points that were correctly labelled
     """
-    x = np.hstack((x_labeled, x_unlabeled, x_test))
+    x = np.vstack((x_labeled, x_unlabeled, x_test))
     k = kernel(x)
     k_ = k[:len(x_labeled), -len(x_test):]
-    y_prediction = (y[None, :] @ k_)[0]
+    y_prediction = np.where((y[None, :] @ k_)[0] >= 0, 1, -1)
     acc = np.sum(y_prediction == y_test) / len(y_test)
     return acc
 
@@ -35,12 +35,12 @@ def evaluate_kernel_SVM(x_labeled, x_unlabeled, x_test, y, y_test, kernel):
     """
     n_train = len(x_labeled)
     n_test = len(x_test)
-    x = np.hstack((x_labeled, x_unlabeled, x_test))
+    x = np.vstack((x_labeled, x_unlabeled, x_test))
     k = kernel(x)
 
-    svc = SVC(C=0, kernel='precomputed')
+    svc = SVC(C=10, kernel='precomputed')
     svc.fit(k[:n_train, :n_train], y)
-    y_prediction = svc.predict(k[-n_test:, -n_test:])
+    y_prediction = svc.predict(k[-n_test:, :n_train])
     acc = np.sum(y_prediction == y_test) / len(y_test)
     return acc
 
@@ -65,14 +65,29 @@ def get_data():
     x_win = read_data_folder(os.path.join(Path().absolute(), '20news-18828', 'comp.windows.x'))
     data = x_mac + x_win
 
-    y_mac = np.zeros(len(x_mac))
+    y_mac = -np.ones(len(x_mac))
     y_win = np.ones(len(x_win))
     y = np.hstack((y_mac, y_win))
 
-    vect = TfidfVectorizer(min_df=4, smooth_idf=False, encoding='latin-1', norm=None)
-    x = vect.fit_transform(data)
-    return x, y
+    vect = TfidfVectorizer(min_df=4, smooth_idf=False, encoding='latin-1')
+    vect.fit(data)
+    x_mac = vect.transform(x_mac).toarray()
+    x_win = vect.transform(x_win).toarray()
+
+    i = np.random.permutation(np.arange(x_mac.shape[0]))
+    j = np.random.permutation(np.arange(x_win.shape[0]))
+    return x_mac[i], x_win[j], y_mac[i], y_win[j]
 
 
 if __name__ == '__main__':
-    x, y = get_data()
+    x_mac, x_win, y_mac, y_win = get_data()
+    l = 50
+    x_labeled = np.vstack((x_mac[:l], x_win[:l]))
+    x_test = np.vstack((x_mac[l:], x_win[l:]))
+    y = np.hstack((y_mac[:l], y_win[:l]))
+    y_test = np.hstack((y_mac[l:], y_win[l:]))
+
+    kernel = lambda x: clustered_representation.kernel(x, 5)
+    acc = evaluate_kernel(x_labeled, np.zeros((0, x_mac.shape[1])),
+                              x_test, y, y_test, kernel)
+    print(acc)
